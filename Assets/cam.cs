5079aa2ca6540731c335326700102e23d7b94855
Russell Dunk <russelldunk@gmail.com>
2022-03-05 07:37:35 -0500

    Prog gen islands!

--- Assets/cam.cs
+++ Assets/cam.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
 
-public class cam : MonoBehaviour
+public class Cam : MonoBehaviour
 {
+    public GameObject island;
     // Start is called before the first frame update
+    public int numIslands = 100;
+    public int scale = 1000; 
     void Start()
     {
-        Debug.Log("test");
-        Debug.Log("test");
+        for (var i = 0; i < numIslands; i++)
+        {
+            float x = UnityEngine.Random.value * scale;
+            float y = UnityEngine.Random.value * scale;
+            float z = UnityEngine.Random.value * scale;
+            Instantiate(island, new Vector3(x, y, z), Quaternion.identity);
+            
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("test");
     }
 }
